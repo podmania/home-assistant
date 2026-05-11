834b44c71815dc5755642386b252dd5f4bfa8279
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-05-10 22:55:26 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
           pkgs = nixpkgs.legacyPackages.${system};
           n2c = nix2container.outputs.packages.${system}.nix2container;
 
-          # Dummy hashes
+          # Dummy SRI hashes (updated by CI)
           haosImg_x86_64 = pkgs.fetchurl {
             url = "https://github.com/home-assistant/operating-system/releases/download/17.3/haos_generic-x86-64-17.3.img.xz";
             hash = "sha256-0000000000000000000000000000000000000000000000000000=";
@@ -30,15 +30,65 @@
             let
               extraPackages = with pkgs; [ qemu OVMF virtiofsd xz ];
 
-              entrypoint = pkgs.writeShellScript "entrypoint.sh" (builtins.readFile ./entrypoint.sh);
+              # Generate entrypoint script with all paths interpolated
+              entrypointScript = pkgs.writeShellScript "entrypoint.sh" ''
+                #!${pkgs.bash}/bin/bash
+                set -e
 
-              entrypointFixed = pkgs.runCommand "entrypoint-fixed" {} ''
-                sed -e "s|\${OVMF_FD}|${pkgs.OVMF.fd}/FV|g" \
-                    -e "s|\${OVMF_VARS}|${pkgs.OVMF.fd}/FV|g" \
-                    -e "s|\${haosSource}|${haosSource}|g" \
-                    -e "s|isX86=\"false\"|isX86=\"${if isX86 then "true" else "false"}\"|g" \
-                    ${entrypoint} > $out
-                chmod +x $out
+                SHARED_DIR="/config"
+                SOCKET_PATH="/run/virtiofsd/ha.sock"
+                mkdir -p "$SHARED_DIR" "$(dirname $SOCKET_PATH)"
+
+                virtiofsd --socket-path="$SOCKET_PATH" --shared-dir="$SHARED_DIR" --cache=auto --sandbox=chroot --syslog &
+                VIRTIOFSD_PID=$!
+                while [ ! -S "$SOCKET_PATH" ]; do
+                  if ! kill -0 $VIRTIOFSD_PID 2>/dev/null; then exit 1; fi
+                  sleep 0.1
+                done
+
+                IMAGE_XZ="/storage/haos.img.xz"
+                IMAGE_QCOW2="/storage/haos.qcow2"
+                if [ ! -f "$IMAGE_XZ" ]; then
+                  cp ${haosSource} "$IMAGE_XZ"
+                fi
+                if [ ! -f "$IMAGE_QCOW2" ]; then
+                  unxz -k "$IMAGE_XZ"
+                  if [ "${if isX86 then "true" else "false"}" = "true" ]; then
+                    qemu-img convert -f raw -O qcow2 "''${IMAGE_XZ%.xz}" "$IMAGE_QCOW2"
+                    rm "''${IMAGE_XZ%.xz}"
+                  else
+                    mv "''${IMAGE_XZ%.xz}" "$IMAGE_QCOW2"
+                  fi
+                fi
+
+                KVM_ARGS=""
+                if [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ]; then
+                  KVM_ARGS="-enable-kvm -cpu host"
+                fi
+
+                QEMU_USB_ARGS=""
+                if [ -d "/passthrough" ]; then
+                  QEMU_USB_ARGS="-usb"
+                  while IFS= read -r -d '' dev; do
+                    if [ -c "$dev" ] || [ -b "$dev" ]; then
+                      QEMU_USB_ARGS="$QEMU_USB_ARGS -device usb-host,hostdevice=$dev"
+                    fi
+                  done < <(find /passthrough -type c -o -type b -print0 2>/dev/null || true)
+                fi
+
+                exec qemu-system-${arch} $KVM_ARGS \
+                  -M q35 \
+                  -smp cores="''${CPU_CORES:-2}" \
+                  -m "''${RAM_SIZE:-4096}" \
+                  -drive file="$IMAGE_QCOW2",if=virtio,cache=unsafe,aio=native \
+                  -drive file=${pkgs.OVMF.fd}/FV/OVMF.fd,if=pflash,format=raw,unit=0 \
+                  -drive file=${pkgs.OVMF.fd}/FV/OVMF_VARS.fd,if=pflash,format=raw,unit=1 \
+                  -netdev user,id=net0,hostfwd=tcp::''${FORWARD_PORT:-8123}-:8123 \
+                  -device virtio-net-pci,netdev=net0 \
+                  -chardev socket,id=char0,path="$SOCKET_PATH" \
+                  -device vhost-user-fs-pci,queue-size=1024,chardev=char0,tag=config \
+                  $QEMU_USB_ARGS \
+                  -nographic -monitor none -serial mon:stdio
               '';
 
               baseEnv = pkgs.buildEnv {
@@ -49,11 +99,10 @@
 
               extraLayers = pkgs.symlinkJoin {
                 name = "haos-extra";
-                paths = [ baseEnv entrypointFixed ];
-                buildInputs = [ pkgs.coreutils ];
+                paths = [ baseEnv entrypointScript ];
                 postBuild = ''
                   mkdir -p $out/run/virtiofsd
-                  ln -sf ${entrypointFixed} $out/bin/entrypoint.sh
+                  ln -sf ${entrypointScript} $out/bin/entrypoint.sh
                   ln -sf ${pkgs.bashInteractive}/bin/bash $out/bin/sh
                 '';
               };
